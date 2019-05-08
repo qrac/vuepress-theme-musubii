e9e85542beb9f33817135e8d47b13070ed90e050
Qrac <qrac.jp@gmail.com>
2019-05-08 20:14:01 +0900

    Dark theme button

--- components/DarkThemeButton.vue
+++ components/DarkThemeButton.vue
@@ -1,9 +1,49 @@
 <template>
-  <button class="button is-plain is-circle is-sm" type="button">D</button>
+  <div class="toggle-theme-button">
+    <input type="checkbox" class="input is-hidden" id="theme-toggle" v-model="darkTheme">
+    <label class="button is-outline is-circle is-sm" for="theme-toggle">D</label>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      darkTheme: this.initDarkTheme()
+    };
+  },
+  methods: {
+    initDarkTheme: function() {
+      const darkTheme = localStorage.getItem("darkTheme");
+      if (darkTheme === null) {
+        //console.log("Nothing darkTheme; default to `false`");
+        return false;
+      } else {
+        //console.log("Using darkTheme value " + darkTheme);
+        return darkTheme == "true";
+      }
+    }
+  },
+  mounted() {
+    //console.log("Dark Theme Mounted: " + this.darkTheme);
+    if (this.darkTheme === true) {
+      document.body.setAttribute("data-theme", "dark");
+    } else {
+      document.body.setAttribute("data-theme", "light");
+    }
+  },
+  watch: {
+    darkTheme: function() {
+      localStorage.setItem("darkTheme", JSON.stringify(this.darkTheme));
+      //console.log("Dark Theme: " + JSON.stringify(this.darkTheme));
+      if (this.darkTheme === true) {
+        document.body.setAttribute("data-theme", "dark");
+      } else {
+        document.body.setAttribute("data-theme", "light");
+      }
+    }
+  }
+};
 </script>
 
 
