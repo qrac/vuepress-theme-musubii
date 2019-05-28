ae18cb923f03228ef02d9d09b9e106879f4183e0
Qrac <qrac.jp@gmail.com>
2019-05-28 11:28:30 +0900

    Test dark variables

--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -39,7 +39,8 @@ export default {
   computed: {
     ...mapGetters(["getDarkTheme"]),
     setDataTheme() {
-      if (this.getDarkTheme === true) {
+      const darkTheme = this.getDarkTheme;
+      if (darkTheme === true) {
         return "dark";
       } else {
         return "light";
