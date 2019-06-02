d36c63227c30a9b3a5cb19ab678bbaf9eb27ad81
Qrac <qrac.jp@gmail.com>
2019-06-02 21:12:56 +0900

    License

--- components/libraryPkgInfo.vue
+++ components/libraryPkgInfo.vue
@@ -2,6 +2,7 @@
   <p class="text is-light is-center is-sm is-strong is-font-sans-en" v-if="libraryPkgFile">
     <i class="fab fa-github" aria-hidden="true"></i>
     <span class="text" v-if="libraryPkgFile.version">v{{ libraryPkgFile.version }}・</span>
+    <span class="text" v-if="libraryPkgFile.license">{{ libraryPkgFile.license }}・</span>
     <a class="text is-link-reverse" :href="libraryPkgFile.repository">
       <span class="text">Repository</span>
     </a>
